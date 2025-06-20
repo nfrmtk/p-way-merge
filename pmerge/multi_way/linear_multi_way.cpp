f2d726ff164c89872743e0533233269fdb382a6d
nfrmtk <nfrmtk.aka.fkfka@gmail.com>
2025-06-20 13:01:06 +0300

    Feature/remove dead code (#1)
    various unused code

--- pmerge/multi_way/linear_multi_way.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by nfrmtk on 3/15/25.
-//
-#include <pmerge/multi_way/linear_multi_way.hpp>
-void pmerge::multi_way::LinearMultiWayMerger::Merge(
-    const std::vector<pmerge::fs::RecordsSource>& sources,
-    const std::filesystem::path& out) {}
