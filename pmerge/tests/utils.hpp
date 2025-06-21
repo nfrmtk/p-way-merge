04960512f38742ecfb7a91aae1097516a2a83b5a
Ivan Lykov <nfrmtk.aka.fkfka@gmail.com>
2025-06-21 17:27:30 +0300

    work

--- pmerge/tests/utils.hpp
+++ pmerge/tests/utils.hpp
@@ -7,16 +7,80 @@
 
 #include <immintrin.h>
 
+#include <algorithm>
 #include <cstdint>
+#include <memory>
 #include <ostream>
 #include <random>
 #include <vector>
+#include <pmerge/ydb/spilling_mem.h>
+#include <pmerge/simd/utils.hpp>
 namespace std {
 inline std::ostream& operator<<(std::ostream& os, __m256i reg) {
   os << pmerge::simd::ToString(reg);
   return os;
 }
 }  // namespace std
+
+TSpillingBlock Make(TSpilling& stats, std::mt19937_64& gen, uint64_t size_blocks){
+  
+  auto storage = std::make_unique<uint64_t[]>(size_blocks);
+  std::uniform_int_distribution<uint64_t> keys_distr()
+  for()
+}
+
+
+template<ui32 keyCount>
+bool Equal(std::span<const pmerge::ydb::Slot, keyCount>first, std::span<const pmerge::ydb::Slot, keyCount>second ) {
+  for (int idx = 0; idx < keyCount; ++idx) {
+    if (first[idx] != second[idx]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+template<ui32 keyCount>
+bool Less(std::span<const pmerge::ydb::Slot, keyCount>first, std::span<const pmerge::ydb::Slot, keyCount>second ) {
+  for (int idx = 0; idx < keyCount; ++idx) {
+    if (first[idx] > second[idx]) {
+      return false;
+    }
+    if (first[idx] < second[idx]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// it is transitive: for all a, b and c, if r(a, b) and r(b, c) are both true then r(a, c) is true;
+// a = (2, 123, 4)
+// b = (1, 3, 7)
+// c = (1, 4, 312)
+// a > c > b 
+
+template<ui32 keyCount>
+bool SlotLess(const pmerge::ydb::Slot& first, const pmerge::ydb::Slot& second) {
+  if (pmerge::ydb::GetHash(first) < pmerge::ydb::GetHash(second)) {
+    return true;
+  }
+  if (pmerge::ydb::GetHash(first) > pmerge::ydb::GetHash(second)) {
+    return false;
+  }
+  auto first_span = pmerge::ydb::GetKey<keyCount>(first);
+  auto second_span = pmerge::ydb::GetKey<keyCount>(second);
+  if (Equal(first_span, second_span)) {
+    return false;
+  }
+  if (Less(first_span, second_span) ) {
+    return true;
+  }else {
+    PMERGE_ASSERT(std::ranges::lexicographical_compare(second_span, first_span));
+    return false;
+  }
+}
+
+
 
 inline void GetComplimentary(const uint64_t* arr, uint64_t* dst) {
   int out_idx = 0;
